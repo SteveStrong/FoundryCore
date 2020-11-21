4fbb51efaeef42fcb3d076b56258593258b14b5d
SteveStrong <sstrong@apprenticesystems.com>
2020-11-21 15:19:05 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoundryCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prop1 = new FoProperty<string>
+            {
+                Value = "Steve Strong"
+            };
+            var prop2 = new FoProperty<double>(100);
+
+            string[] stuff = { "one", "two", "three" };
+            var prop3 = new FoCollection<string>(stuff);
+
+
+            var prop4 = new FoCollection<string>
+            {
+                Value = new List<string> { "one", "two" }
+            };
+
+            Console.WriteLine($" to string {prop1}");
+            Console.WriteLine($" to string {prop2}");
+            Console.WriteLine($" to string {prop3}");
+            Console.WriteLine($" to string {prop4}");
+        }
+    }
+}
---
+++ foProperty.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoundryCore
+{
+    public static class Extensions
+    {
+        public static String Blank(this String me)
+        {
+            return String.Empty;
+        }
+        public static T Blank<T>(this T me)
+        {
+            var tot = typeof(T);
+            return tot.IsValueType
+              ? default(T)
+              : (T)Activator.CreateInstance(tot)
+              ;
+        }
+    }
+
+    class FoBase
+    {
+        public virtual string AsString() { return "FoBase"; }
+
+        public override string ToString()
+        {
+            return AsString();
+        }
+    }
+    class FoProperty<T> : FoBase
+    {
+        public T Value { get; set; }
+
+        public FoProperty(T value = default)
+        {
+            this.Value = value;
+        }
+
+        public override string AsString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    class FoCollection<T> : FoProperty<T>
+    {
+        public new List<T> Value { get; set; }
+
+        public FoCollection(T[] value = default) 
+        {
+            Value = value == default ? new List<T>() :new List<T>(value);
+        }
+
+        public override string AsString()
+        {
+            return string.Join(",", Value); 
+        }
+    }
+}
