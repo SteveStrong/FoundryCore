7a0051a75b45ce750835dda30f33457b7cd6747b
SteveStrong <sstrong@apprenticesystems.com>
2020-11-26 22:32:21 -0500

    parser works

--- Exceptions/ApprenticeSyntaxException.cs
+++ Exceptions/ApprenticeSyntaxException.cs
@@ -7,26 +7,26 @@ namespace FoundryCore
 	/// </summary>
 	public class ApprenticeSyntaxException : ApprenticeException
 	{
-		private readonly string m_sTitle;
-		private readonly string m_sBuffer;
+		private readonly string _Title;
+		private readonly string _Buffer;
 
 		public ApprenticeSyntaxException() : base()
 		{
 		}
 		public ApprenticeSyntaxException(string sTitle, string sBuffer) : base("Expression not Found")
 		{
-			m_sTitle = sTitle;
-			m_sBuffer = sBuffer;
+			_Title = sTitle;
+			_Buffer = sBuffer;
 		}
 		public ApprenticeSyntaxException(string sTitle, string sBuffer, Token oTok) : base(oTok.Text)
 		{
-			m_sTitle = sTitle;
-			m_sBuffer = sBuffer;
+			_Title = sTitle;
+			_Buffer = sBuffer;
 		}
 		public ApprenticeSyntaxException(string sTitle, string sBuffer, string sMessage) : base(string.Format("Expecting a {0}",sMessage))
 		{
-			m_sTitle = sTitle;
-			m_sBuffer = sBuffer;
+			_Title = sTitle;
+			_Buffer = sBuffer;
 		}
 		public override string DisplayCaption
 		{
@@ -41,16 +41,16 @@ namespace FoundryCore
 			{
 				string sLoc = base.DisplayMessage;
 				if ( sLoc != "" )
-					return m_sTitle + ": " + m_sBuffer + " Loc:" + sLoc;
+					return _Title + ": " + _Buffer + " Loc:" + sLoc;
 				else
-					return m_sTitle + ": " + m_sBuffer;
+					return _Title + ": " + _Buffer;
 			}
 		}
 		public override string DisplayMessage
 		{
 			get 
 			{
-				return m_sTitle + ": " + m_sBuffer + "/r" + base.DisplayMessage;
+				return _Title + ": " + _Buffer + "/r" + base.DisplayMessage;
 			}
 		}
 	}
