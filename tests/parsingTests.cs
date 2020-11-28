15b32a65a112e45782f9f24a42a407830a76bfaf
SteveStrong <sstrong@apprenticesystems.com>
2020-11-28 12:13:01 -0500

    new version that does evaluation

--- tests/parsingTests.cs
+++ tests/parsingTests.cs
@@ -22,19 +22,27 @@ namespace FoundryCore
             SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(@"
                 using System;
 
-                namespace RoslynCompileSample
+                namespace Apprentice
                 {
                     public class Writer
                     {
-                        public void Write(string message)
+                        public void Write(string message, int count)
                         {
                             Console.WriteLine(message);
-                              for(int i = 0; i < 10; i++)
+                              for(int i = 0; i < count; i++)
                                 {    
                                     var xxx = i * 2 * 5 + 100;
                                     var str = $""{message} => {xxx}"";
                                     Console.WriteLine(str);
                                 }
+                        }
+
+                        public double Math(double a, double b)
+                        {
+                            var c = a * b;
+                            var str = $""{a} * {b} => {c}"";
+                            Console.WriteLine(str);
+                            return c;
                         }
                     }
                 }");
@@ -52,21 +60,21 @@ namespace FoundryCore
                 .ForEach(a => references.Add(MetadataReference.CreateFromFile(Assembly.Load(a).Location)));
 
             // analyse and generate IL code from syntax tree
-            CSharpCompilation compilation = CSharpCompilation.Create(
+            var compilation = CSharpCompilation.Create(
                 assemblyName,
                 syntaxTrees: new[] { syntaxTree },
                 references: references,
                 options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
-            using (var ms = new MemoryStream())
+            using (var stream = new MemoryStream())
             {
                 // write IL code into memory
-                EmitResult result = compilation.Emit(ms);
+                EmitResult result = compilation.Emit(stream);
 
                 if (!result.Success)
                 {
                     // handle exceptions
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    var failures = result.Diagnostics.Where(diagnostic =>
                         diagnostic.IsWarningAsError ||
                         diagnostic.Severity == DiagnosticSeverity.Error);
 
@@ -78,17 +86,26 @@ namespace FoundryCore
                 else
                 {
                     // load this 'virtual' DLL so that we can use
-                    ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = Assembly.Load(ms.ToArray());
+                    stream.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(stream.ToArray());
 
                     // create instance of the desired class and call the desired function
-                    Type type = assembly.GetType("RoslynCompileSample.Writer");
-                    object obj = Activator.CreateInstance(type);
+                    Type type = assembly.GetType("Apprentice.Writer");
+                    var obj = Activator.CreateInstance(type);
                     type.InvokeMember("Write",
                         BindingFlags.Default | BindingFlags.InvokeMethod,
                         null,
                         obj,
-                        new object[] { "Hello: World" });
+                        new object[] { "Hello: World", 3 });
+
+                    var resultx = type.InvokeMember("Math",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        obj,
+                        new object[] { 17.0, 3.0 });
+
+                    var str = $" resultx => {resultx}";
+                    Console.WriteLine(str);
                 }
             }
 
