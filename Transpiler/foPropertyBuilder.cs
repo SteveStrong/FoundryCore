4cdbb974d4cd3ed1cb498883f35c4d6921b26acf
SteveStrong <sstrong@apprenticesystems.com>
2020-11-30 20:14:31 -0500

    new property builder

---
+++ Transpiler/foPropertyBuilder.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+
+namespace FoundryCore
+{
+    public interface  IFoPropertyBuilder {
+        public IFoPropertyBuilder SetName(string name);
+        public IFoPropertyBuilder SetValue(object value);
+        public FoComponent Build();
+     }
+    public class FoPropertyBuilder : IFoPropertyBuilder
+    {
+        private string _name;
+        private object _value;
+
+        public static IFoPropertyBuilder Start() {
+            var builder = new FoPropertyBuilder();
+            return (IFoPropertyBuilder)builder;
+        }
+        public IFoPropertyBuilder SetName(string name){
+            this._name = name;
+            return (IFoPropertyBuilder)this;
+        }
+
+        public IFoPropertyBuilder SetValue(object value) {
+            this._value = value;
+            return (IFoPropertyBuilder)this;
+        }
+        public FoProperty<T> Build<T>(){
+            var result = new FoProperty<T>(this._name);
+            return result;
+        }
+        
+    }
+}
